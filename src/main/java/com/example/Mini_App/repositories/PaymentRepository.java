e8a179792b553629d9ab12540464a5013711ec8a
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-04-20 18:59:23 +0200

    Updated Payment Repo
    Added findbyamount greater than

--- src/main/java/com/example/Mini_App/repositories/PaymentRepository.java
+++ src/main/java/com/example/Mini_App/repositories/PaymentRepository.java
@@ -3,6 +3,11 @@ package com.example.Mini_App.repositories;
 import com.example.Mini_App.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // Finding payments with an amount above a set threshold.
+    List<Payment> findByAmountGreaterThan(double threshold);
+
     
 }
