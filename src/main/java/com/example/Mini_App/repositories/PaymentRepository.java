92f251b2c0e62c795491dfda689b6eeff3f1ded1
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-04-20 19:12:16 +0200

    Payment Servce
    Added Payment Service Methods

--- src/main/java/com/example/Mini_App/repositories/PaymentRepository.java
+++ src/main/java/com/example/Mini_App/repositories/PaymentRepository.java
@@ -2,12 +2,13 @@ package com.example.Mini_App.repositories;
 
 import com.example.Mini_App.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     // Finding payments with an amount above a set threshold.
     List<Payment> findByAmountGreaterThan(double threshold);
 
-    
+
 }
--- src/main/java/com/example/Mini_App/services/PaymentService.java
+++ src/main/java/com/example/Mini_App/services/PaymentService.java
@@ -1,6 +1,44 @@
 package com.example.Mini_App.services;
 
+import com.example.Mini_App.models.Payment;
+import com.example.Mini_App.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PaymentService {
-    
-    
+
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Payment addPayment(Payment payment){
+        return paymentRepository.save(payment);
+    }
+    public List<Payment> getAllPayments(){
+        return paymentRepository.findAll();
+    }
+
+    public Payment getPaymentById(Long id){
+        return paymentRepository.findById(id).orElse(null);
+    }
+    public Payment updatePayment(Long id, Payment payment){
+        Payment existingPayment = paymentRepository.findById(id).orElse(null);
+        if (existingPayment != null) {
+            existingPayment = payment;
+        }
+        return null;
+    }
+    public void deletePayment(Long id){
+        paymentRepository.deleteById(id);
+    }
+    public List<Payment> findByAmountThreshold(Double threshold){
+        return paymentRepository.findByAmountGreaterThan(threshold);
+    }
+
 }
