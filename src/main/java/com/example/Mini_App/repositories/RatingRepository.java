cd33ae4633f76b307b90cc4890669ad18e9fcd55
Ahmad Safwat <ahmad.safwat.elewa@gmail.com>
2025-04-22 23:13:28 +0200

    Finished Ratings

--- src/main/java/com/example/Mini_App/repositories/RatingRepository.java
+++ src/main/java/com/example/Mini_App/repositories/RatingRepository.java
@@ -1,5 +1,11 @@
 package com.example.Mini_App.repositories;
 
-public class RatingRepository {
-    
+import com.example.Mini_App.models.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends MongoRepository<Rating, String> {
+    List<Rating> findByIdAndAndEntityType(Long id, String entityType);
+    List<Rating> findByScoreGreaterThan(Integer score);
 }
--- src/main/java/com/example/Mini_App/services/RatingService.java
+++ src/main/java/com/example/Mini_App/services/RatingService.java
@@ -1,5 +1,38 @@
 package com.example.Mini_App.services;
 
+import com.example.Mini_App.models.Rating;
+import com.example.Mini_App.repositories.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RatingService {
-    
+    private final RatingRepository ratingRepository;
+    @Autowired
+    public RatingService(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    public Rating addRating(Rating rating) {
+        return ratingRepository.save(rating);
+    }
+
+    public Rating updateRating(String id, Rating updatedRating){
+        updatedRating.setId(id);
+        return ratingRepository.save(updatedRating);
+    }
+
+    public void deleteRating(String id){
+        ratingRepository.deleteById(id);
+    }
+
+    public List<Rating> getRatingsByEntity(Long entityId, String entityType){
+        return ratingRepository.findByIdAndAndEntityType(entityId, entityType);
+    }
+
+    public List<Rating> findRatingsAboveScore(int minScore){
+        return ratingRepository.findByScoreGreaterThan(minScore);
+    }
 }
